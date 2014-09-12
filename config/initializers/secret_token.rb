76fb851d701fa402910385404c349439b57510c7
mohamed.shajith <mohamed.shajith@lakesidelab.com>
2014-09-12 19:03:15 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logix::Application.config.secret_token = '75331a12249ef178fc17617e0939f46ba0de94d783fa91037bf96ba1e48c97148a308aac32aad6aca73118b3e32580dfae4e79682c5bfda6718db78fa75ff7ca'
