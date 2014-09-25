ed7bb2cf7bec2ad98f8df9b0bfb4ab7811975c38
mohamed.shajith <mohamed.shajith@lakesidelab.com>
2014-09-25 14:11:00 +0530

    added analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Logix::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  GA.tracker = "UA-55160416-1"
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
